d901cee02706ecaa841aff586ab61e6557c19258
gilmariosoftware@gmail.com <gilmariosoftware@gmail.com>
2018-06-04 23:07:15 -0300

    Inicio do Layout

---
+++ src/core/components/header.component.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+class Header extends Component{
+
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <header className="container is-fluid header has-background-grey">
+                $Super Financeiro
+            </header>
+        )
+    }
+}
+export default Header
